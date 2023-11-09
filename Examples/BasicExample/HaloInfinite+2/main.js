f4043df3a83092a052e358ddda3c99f43a93c669
Smartisek <domalipdominik@gmail.com>
2023-11-09 12:56:39 +0000

    basic structure added

--- Examples/BasicExample/HaloInfinite+2/main.js
+++ Examples/BasicExample/HaloInfinite+2/main.js
@@ -1,66 +1,48 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const player = document.getElementById("player");
+const enemy = document.getElementById("enemy");
+const gameArea = document.getElementById("game-container");
 
-const playerWidth = 50;
-const playerHeight = 50;
-let playerX = 0;
-let playerY = canvas.height/2 - playerHeight/2;
-const enemyWidth = 50;
-const enemyHeight = 50;
-let enemyX = canvas.width - enemyWidth;
-let enemyY = canvas.height/2 -enemyHeight/2;
+const playerWidth = player.offsetWidth;
+const playerHeight = player.offsetHeight;
+const gameWidth = gameArea.offsetWidth;
+const gameHeight = gameArea.offsetHeight;
 
+let playerX =0;
+let playerY = 75;
+let enemyX = Math.random() * (gameWidth - playerWidth);
+let enemyY = Math.random() * (gameHeight - playerHeight);
+
+let gameOver = false;
 let keys = {};
 
-// main function for game loop
 function gameLoop(){
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-// rect for player 
-    ctx.fillStyle = "purple";
-    ctx.fillRect(playerX, playerY, playerWidth, playerHeight);
+    handlePlayerMovement();
+    moveEnemyTowardsPlayer();
+    updatePlayerPosition();
+    updayeEnemyPosition();
+    checkCollisions();
 
-// rect for enemy
-    ctx.fillStyle = "orange";
-    ctx.fillRect(enemyX, enemyY, enemyWidth, enemyHeight);
-
-    const enemySpeed = 1;
-    const dx = playerX - enemyX; //distance between player and enemy
-    const dy = playerY - enemyY;
-    const distance = Math.sqrt(dx * dx + dy * dy);
-
-    if(distance > 0){
-        enemyX += dx/distance * enemySpeed;
-        enemyY += dy/ distance * enemySpeed;
+    if(!gameOver){
+        requestAnimationFrame(gameLoop);
+    } else {
+        endGame();
     }
-
-    const playerSpeed = 3;
-    
-    if(keys.ArrowLeft && playerX >0){
-        playerX -= playerSpeed;
-    }
-
-    if(keys.ArrowRight && playerX < canvas.width-playerWidth){
-        playerX += playerSpeed;
-    }
-
-    if(keys.ArrowUp && playerY > 0){
-        playerY -= playerSpeed;
-    }
-
-    if(keys.ArrowDown && playerY < canvas.height-playerHeight){
-        playerY += playerSpeed;
-    }
-
-    requestAnimationFrame(gameLoop);
 }
 
-document.addEventListener("keydown", event => {
-    keys[event.key] = true;
-});
+function endGame(){
+    alert("Game Over");
+    resetGame();
+}
 
-document.addEventListener("keyup", event => {
-    keys[event.key] = false;
-});
-
+function resetGame(){
+ playerX =0;
+ playerY = 75;
+ enemyX = Math.random() * (gameWidth - playerWidth);
+ enemyY = Math.random() * (gameHeight - playerHeight);
+keys = {};
+player.style.opacity = 1;
+gameOver = false;
+requestAnimationFrame(gameLoop);
+}
 
 requestAnimationFrame(gameLoop);
