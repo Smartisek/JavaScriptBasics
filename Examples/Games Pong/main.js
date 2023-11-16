6d318075952727a9abaeb76a855daa2b2b98ca80
Smartisek <domalipdominik@gmail.com>
2023-11-16 11:31:51 +0000

    finishing update function plus collision for player 2

--- Examples/Games Pong/main.js
+++ Examples/Games Pong/main.js
@@ -35,6 +35,11 @@ function drawText(text, x, y, color){
     context.fillText(text, x , y);
 }
 
+function drawRect(x,y,width, height, color){
+    context.fillStyle = color;
+    context.fillRect(x, y, width, height);
+}
+
 function reset(){
     ballX = width/2;
     ballY = height/2;
@@ -54,6 +59,13 @@ function collisionDetection(){
         let deltaY = ballY - (player2Y + paddleHeight/2);
         ballSpeedY = deltaY * 0.35;
     }
+
+    if(ballX + ballRadius> width -paddleWith && ballY > player2Y && ballY < player2Y +paddleHeight){
+        ballSpeedX = -ballSpeedX;
+        let deltaY = ballY - (player2Y + paddleHeight/2);
+        ballSpeedY = deltaY * 0.35;
+    }
+
     if(ballY - ballRadius < 0 || ballY + ballRadius > height){
         ballSpeedY = -ballSpeedY;
     }
@@ -67,29 +79,49 @@ function collisionDetection(){
 }
 
 function update(){
+if(upPressed){
+    player1Y-= 5;
+} else if(downPressed){
+    player1Y +=5;
+}
 
+if(wPressed){
+    player2Y -= 5;
+} else if(sPressed){
+    player2Y += 5;
+}
+
+ballX +=ballSpeedX;
+ballY += ballSpeedY;
+
+collisionDetection();
+drawRect(0, 0, width, height, "#eee");
+drawRect(0, player1Y, paddleWith, paddleHeight, "black");
+drawRect(width-paddleWith, player2Y, paddleWith, paddleHeight, "black");
+drawCircle(ballX, ballY, ballRadius, "black");
+drawText(player1Score, width/4, 50, "black");
+drawText(player2Score, 3*width/4, 50, "black");
 }
 
 document.addEventListener("keydown", (event) =>{
     if(event.key === "ArrowUp"){
-        upPressed = true;
-    } else if(event.key === "ArrowDown"){
-        downPressed = true;
-    } else if( event.key === "W"){
-        wPressed = true;
-    } else if(event.key === "S"){
+        upPressed = true; 
+    } else if (event.key === "ArrowDown"){
+        downPressed = true; 
+    } else if (event.key === "w"){
+        wPressed = true; 
+    } else if (event.key === "s"){
         sPressed = true;
     }
 });
-
 document.addEventListener("keyup", (event) =>{
     if(event.key === "ArrowUp"){
-        upPressed = false;
-    } else if(event.key === "ArrowDown"){
-        downPressed = false;
-    } else if( event.key === "W"){
-        wPressed = false;
-    } else if(event.key === "S"){
+        upPressed = false; 
+    } else if (event.key === "ArrowDown"){
+        downPressed = false; 
+    } else if (event.key === "w"){
+        wPressed = false; 
+    } else if (event.key === "s"){
         sPressed = false;
     }
 });
