9b97362f2d83672b6ebd372e45abfc70a6386a5c
Smartisek <domalipdominik@gmail.com>
2023-11-16 12:24:04 +0000

    bullet class

--- Examples/Space Invaders/main.js
+++ Examples/Space Invaders/main.js
@@ -29,6 +29,23 @@ class Player{
     }
 }
 
+class Bullet{
+    constructor(x, y, width, height, speed){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+    }
+    draw(){
+        ctx.fillStyle = "white";
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    update(){
+        this.y -= this.speed;
+    }
+}
+
 const allienRows = 4;
 const allienColumns = 10;
 const allienWidth = 40;
