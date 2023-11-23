99c3ef8adf07ac0f02e96a2ea16d273d8185f6c1
Smartisek <domalipdominik@gmail.com>
2023-11-23 10:55:40 +0000

    creating base component and game object

---
+++ JsGameEngine/js/Engine/component.js
@@ -0,0 +1,16 @@
+class Component{
+    // empty because we will be rewriting 
+    // polymorphisom
+    constructor(){
+        this.gameObject = 0;
+    }
+
+    update(deltaTime){
+
+    }
+
+    draw(ctx){
+
+    }
+}
+export default Component;
