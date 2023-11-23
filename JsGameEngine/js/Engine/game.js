54a119a038e35a542c0053022f95729fb5f86bf3
Smartisek <domalipdominik@gmail.com>
2023-11-23 11:00:49 +0000

    creating game class

---
+++ JsGameEngine/js/Engine/game.js
@@ -0,0 +1,13 @@
+// import camera frin "./camera.js"
+class Game{
+    constructor(canvasId){
+        this.canvas = document.getElementById(canvasId);
+        this.ctx = this.canvas.getContext("2d");
+        this.gameObjectsToRemove = [];
+        this.lastFrameTime = 0;
+        this.deltaTime = 0;
+        this.resizeCanvas();
+        window.addEventListener("resize",() => this.resizeCanvas());
+        // this.camera = new Camera(null, this.canvas.width, this.canvas.height);
+    }
+}
